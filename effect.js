d149347a75217e1444264d988cf4f5b4de4a1292
hbdaya <135622719+hbdaya@users.noreply.github.com>
2023-06-05 21:57:49 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -7,13 +7,13 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-400},500);
+			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-1500},500);
-			$('#b44').animate({top:240, left: vw-100},500);
+			$('#b33').animate({top:240, left: vw-150},500);
+			$('#b44').animate({top:240, left: vw-500},500);
 			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+80},500);
-			$('#b77').animate({top:240, left: vw+1100},500);
+			$('#b66').animate({top:240, left: vw+150},500);
+			$('#b77').animate({top:240, left: vw+100},500);
 		});
 
 	$('#turn_on').click(function(){
